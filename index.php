c23c99fa6310b143958fd453f32ed8f8858bb003
Dirk Brünsicke <d1rk@gmx.de>
2010-09-13 22:09:46 +0200

    Revert "renamed folder"
    This reverts commit c99da2576ba71e631093fa35ea2359d8031355d1.

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 /**
  *  Get Cake's root directory
  */
-	define('APP_DIR', 'groupcount');
+	define('APP_DIR', 'app');
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', dirname(__FILE__));
 	define('WEBROOT_DIR', 'webroot');
