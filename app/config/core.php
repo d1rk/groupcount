225df3dd0e380e2c12b96f5e2907ecd9d5a9857c
Dirk Brünsicke <d1rk@gmx.de>
2010-09-13 21:34:27 +0200

    adapting core.php

--- app/config/core.php
+++ app/config/core.php
@@ -85,7 +85,7 @@
  *
  * [Note Routing.admin is deprecated in 1.3.  Use Routing.prefixes instead]
  */
-	//Configure::write('Routing.prefixes', array('admin'));
+	Configure::write('Routing.prefixes', array('admin'));
 
 /**
  * Turn off all caching application-wide.
@@ -200,12 +200,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUoijf938fu987hgiuhg9jw0e9figubWwvniR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '768593892374982009657453542496749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
